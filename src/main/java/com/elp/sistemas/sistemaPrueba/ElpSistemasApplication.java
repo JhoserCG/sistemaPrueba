7fcd93d9c31ae132166c3383ce4685727e1efd3d
JhoserCG <jhosercordova@gmail.com>
2024-12-27 10:30:54 -0500

    Se sube este proyecto

---
+++ src/main/java/com/elp/sistemas/sistemaPrueba/ElpSistemasApplication.java
@@ -0,0 +1,13 @@
+package com.elp.sistemas.sistemaPrueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElpSistemasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElpSistemasApplication.class, args);
+	}
+
+}
